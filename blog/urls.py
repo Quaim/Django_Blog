41ed2f8e57fbf25da2867111d0f2bb4eed628b3a
Ellis Taylor <ellis.taylor@live.com>
2024-02-24 02:10:25 +0000

    fixed admin issue by moving the url pattern into blog/urls rather than codestar/urls MIGHT NEED TO UNDO AS LMS DIDNT DO THIS

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from . import views
 from django.urls import path
+from django.contrib import admin
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('admin/', admin.site.urls),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
+    
 ]
