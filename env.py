861759de9f80681695d8b5d7112b3e424ffc7adf
Ellis Taylor <ellis.taylor@live.com>
2024-02-24 13:28:03 +0000

    linked signup to base/html to access custom css, added cloudary api key to env.py

--- env.py
+++ env.py
@@ -3,5 +3,8 @@ import os
 os.environ.setdefault(
     "DATABASE_URL", "postgres://esuavgdk:EOhc_aRbLHEogtqjWlk1EluIDtTV3Pn2@lucky.db.elephantsql.com/esuavgdk")
 os.environ.setdefault("SECRET_KEY", "zo~^=UtY!t6RR9@")
+os.environ.setdefault(
+    "CLOUDINARY_URL", "CLOUDINARY_URL=cloudinary://371429349574452:ce9Y-0-wav4lPPOpneOG7TodeEA@dpwjsiyr3")
 
     
+ 
